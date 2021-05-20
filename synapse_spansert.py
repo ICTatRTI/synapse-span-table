2dd544faa0835ad2b65934465e107c82348f6e54
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2021-05-20 14:14:39 -0400

    Complete the update_span_table_definitions function

--- synapse_spansert.py
+++ synapse_spansert.py
@@ -66,18 +66,58 @@ def create_span_table_definitions(syn, projectName, tableName, requiredColumns, 
         table = syn.store(Table(schema, []))
 
 def update_span_table_definitions(syn, projectName, tableName, spanTableDefinitions, requiredColumns, columnLimit):
-    spanTableDefinitionsSynId = syn.findEntityId(SPAN_TABLE_DEFINITIONS, projectName)
-    # @TODO
-    # allColumns = ...
-    # newColumns = ...
-    # tablesWithRoomToGrow = ...
-    # Fill up tables with room with newColumns and update corresponding entry in spanTableDefinitions.
-    # If still have newColumns entries, create tables until used up. Collect new table definitions as we go.
+    # Find all columns currently in definitions.
+    currentColumns = []
+    for spanTableDefinition in spanTableDefinitions :
+        currentColumns = currentColumns + spanTableDefinition['columns']
+    # Of the requiredColumns, figure out which ones need to be added given currentColumns.
+    columnsToAdd = []
+    for requiredColumn in requiredColumns :
+        if (requiredColumn not in currentColumns) :
+            columnsToAdd.append(requiredColumn)
+    # Fill up span tables with room with newColumns and update corresponding entry in spanTableDefinitions.
+    for spanTableDefinition in spanTableDefinitions :
+        while len(spanTableDefinition['columns']) < columnLimit and len(columnsToAdd) > 0 :
+            columnName = columnsToAdd.pop()
+            spanTableDefinition['columns'].append(columnName)
+            newColumn = syn.store(Column(name=columnName, columnType='LARGETEXT'))
+            synId = syn.findEntityId(spanTableDefinition['spanTableName'], projectName)
+            schema = syn.get(synId)
+            schema.addColumn(newColumn)
+            syn.store(schema)
+    # All span tables filled up and still columns to add? Lets create some more span tables.
+    while len(columnsToAdd) > 0 :
+        # Add the span table to the definitions list.
+        spanTableDefinition = {
+            "tableName": tableName,
+            "spanTableName": tableName + "_" + str(len(spanTableDefinitions) + 1),
+            "columns": ['id']
+        }
+        while len(spanTableDefinition['columns']) < columnLimit and len(columnsToAdd) > 0:
+            spanTableDefinition['columns'].append(columnsToAdd.pop())
+        spanTableDefinitions.append(spanTableDefinition)
+        # Now actually create the span table.
+        columns = []
+        for column in spanTableDefinition['columns'] :
+            columns.append(Column(name=column, columnType='LARGETEXT'))
+        schema = Schema(spanTableDefinition['spanTableName'],
+                        columns,
+                        parent=projectName)
+        syn.store(Table(schema, []))
     # Save updated spanTableDefinitions to SPAN_TABLE_DEFINITIONS table.
+    spanTableDefinitionsSynId = syn.findEntityId(SPAN_TABLE_DEFINITIONS, projectName)
+    row = syn.tableQuery("select * from " + spanTableDefinitionsSynId + " where tableName='" + tableName + "'",
+                         resultsAs="rowset", limit=1)
+    syn.delete(row)
+    spanTableDefinitionsSchema = syn.get(spanTableDefinitionsSynId)
+    data = pd.DataFrame([
+        {
+            "tableName": tableName,
+            "spanTableDefinitions": json.dumps(spanTableDefinitions)
+        }
+    ])
+    syn.store(Table(spanTableDefinitionsSchema, data))
 
-    #bday_column = syn.store(Column(name='birthday', columnType='DATE'))
-    #schema.addColumn(bday_column)
-    #schema = syn.store(schema)
 
 def upsert_span_table_data(syn, projectName, tableName, data) :
     # @TODO
