5d865431807b7f13ab0fa771246734d1fca19407
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2021-05-24 15:56:33 -0400

    Finish writing first 2 tests

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import synapseclient
 from synapseclient import Project
 import json
 import os
-from synapse_span_table import flexsert_span_table_record, install_span_table
+from synapse_span_table import flexsert_span_table_record, install_span_table, read_span_table_record
 
 config = configparser.ConfigParser()
 config.read(os.path.join(os.getcwd(), 'config.ini'))
@@ -27,8 +27,11 @@ def cleanup() :
   delete_all_entities_in_project(syn, synProjectName)
 
 
-install_span_table(syn, synProjectName)
 
+
+TestName = 'Should insert records with different schemas into the same table.'
+tableName = 'test'
+columnLimit = 3
 data1 = {
   "id": "1",
   "a": "1",
@@ -39,7 +42,6 @@ data1 = {
   "f": "1",
   "g": "1"
 }
-
 data2 = {
   "id": "2",
   "a": "2",
@@ -53,16 +55,53 @@ data2 = {
   "i": "2",
   "j": "2"
 }
-
-tableName = 'test'
-columnLimit = 3
-
-# Test inserting records with different schemas into the same table.
+install_span_table(syn, synProjectName)
 flexsert_span_table_record(syn, synProjectName, tableName, data1, columnLimit)
 flexsert_span_table_record(syn, synProjectName, tableName, data2, columnLimit)
-#@TODO Write test conditions.
-#cleanup()
+record1 = read_span_table_record(syn, synProjectName, tableName, data1['id'])
+record2 = read_span_table_record(syn, synProjectName, tableName, data2['id'])
+allValuesMatch = True
+for key in data1.keys() :
+  if record1[key] != data1[key] :
+    allValuesMatch = False
+for key in data2.keys():
+  if record2[key] != data2[key]:
+    allValuesMatch = False
+if allValuesMatch == True :
+  print('Passed: ' + TestName)
+else :
+  print('Failed: ' + TestName)
+cleanup()
 
-# @TODO Test updating a record with the same schema.
+TestName = 'Should update a record with the same schema.'
+tableName = 'test'
+columnLimit = 3
+data = {
+  "id": "1",
+  "a": "1",
+  "b": "1",
+  "c": "1",
+  "d": "1",
+  "e": "1",
+  "f": "1",
+  "g": "1"
+}
+install_span_table(syn, synProjectName)
+flexsert_span_table_record(syn, synProjectName, tableName, data, columnLimit)
+data['g'] = '1a'
+flexsert_span_table_record(syn, synProjectName, tableName, data, columnLimit)
+recordUpdated = read_span_table_record(syn, synProjectName, tableName, data['id'])
+if recordUpdated['g'] == '1a' :
+  print('Passed: ' + TestName)
+else :
+  print('Failed: ' + TestName)
+cleanup()
+
 # @TODO Test updating a record with a different schema.
-# Clean up test data.
+
+# Should properly handle other types of data
+
+# @TODO Test deleting a record.
+
+# @TODO Clean up test data.
+#cleanup()
